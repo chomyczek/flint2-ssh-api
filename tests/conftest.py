cc83e45c4684a17d8d2a1c2149e8013ada941d32
chomyczek <kaszubowski.adam@gmail.com>
2026-08-24 16:26:06 +0200

    add unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,15 @@
+import pytest
+
+from app.services.cache_service import _lock, _cache
+
 TEST_IP = "203.0.113.42"
 TEST_MAC = "aa:bb:cc:dd:ee:ff"
+
+
+@pytest.fixture(autouse=True)
+def clear_cache():
+    with _lock:
+        _cache.clear()
+    yield
+    with _lock:
+        _cache.clear()
